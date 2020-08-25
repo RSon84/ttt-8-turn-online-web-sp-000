d2956ed30ef43b09a67dc375e7a69862471c8aaa
Devin Wilson <craigvin84@yahoo.com>
2020-08-25 00:10:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,36 +1,47 @@
+
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
 
-def valid_move?(board, position)
-  if position.to_i.between?(1,9)
-    if !position_taken?(board, position.to_i-1)
-      true
-    end
+ def input_to_index(input)
+  input.to_i - 1
+ end
+
+
+ def move(board, position_selection, current_player = "X")
+   board[position_selection] = current_player
+ end
+
+ def position_taken?(board, index)
+   if board[index] != ("X") && board[index] != ("O")
+     false
+   elsif board[index] == ("X") || board[index] == ("O")
+     true
+   end
+ end
+
+ def valid_move?(board, index)
+  if index.between?(0, 8) && !(position_taken?(board, index))
+    true
+  else !index.between?(0, 8) || (position_taken?(board, index))
+     false
   end
-
-  # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
+
+
 
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  if valid_move?(board, input)
-    move(board, input)
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
   else
     turn(board)
   end
   display_board(board)
-end
-
-def position_taken?(board, location)
-  board[location] != " "
-end
-
-def move(board, location, current_player = "X")
-  board[location.to_i-1] = current_player
 end
